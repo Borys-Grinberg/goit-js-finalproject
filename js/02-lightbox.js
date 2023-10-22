4fb402a58b1de377b3b670c7f31e16adf95782f8
Borys-Grinberg <borys.grinberg@gmail.com>
2023-10-22 15:30:17 +0200

    task2 first commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Функція для створення рядка HTML за шаблоном
+function createGalleryItemHTML(item) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `;
+}
+
+// Рендер розмітки галереї
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsHTML = galleryItems.map(createGalleryItemHTML).join("");
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsHTML);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt", // Вказати, що відображати у підпису (використовує значення атрибуту alt)
+  captionPosition: "bottom", // Вказати позицію підпису (знизу)
+  captionsDelay: 250, // Затримка перед відображенням підпису
+});
